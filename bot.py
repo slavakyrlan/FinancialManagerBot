d3ce76c215da5b53c33ed5cbcb7c1000cdcdb807
slavakyrlan <slavakyrlan00@mail.ru>
2024-10-10 21:55:03 +0300

    T4 button income

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import logging
 import os
+import sqlite3
+
 import requests
 
 from dotenv import load_dotenv
 from telebot import TeleBot, types
-
+from database import db_connect
 
 load_dotenv()
 
@@ -20,6 +22,10 @@ BUTTONS = ["Доход", "Расход", "Статистика"]
 BUTTONS_EDIT = ["Добавить", "Редактировать", "Удалить", "Назад"]
 
 
+con = db_connect()
+cur = con.cursor()
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
     name = message.from_user.first_name
@@ -31,13 +37,13 @@ def start(message):
         text=f'Привет, {name}. Выберите опцию:',
         reply_markup=keyboard,
     )
+    add_user(message.chat.id)
 
 
 @bot.message_handler(func=lambda message: message.text in BUTTONS)
 def handle_option(message):
     if message.text == "Доход":
-        print('Доход')
-        handle_income_option(message)
+        income_action(message)
     elif message.text == "Расход":
         print('Расход')
     elif message.text == "Статистика":
@@ -47,7 +53,7 @@ def handle_option(message):
                  "'YYYY-MM-DD to YYYY-MM-DD'")
 
 
-def handle_income_option(message):
+def income_action(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*BUTTONS_EDIT)
     bot.send_message(
@@ -55,7 +61,162 @@ def handle_income_option(message):
         text="Выберите действие с доходом:",
         reply_markup=keyboard,
     )
+    bot.register_next_step_handler(message, process_income_action)
 
+
+def process_income_action(message):
+    if message.text == "Добавить":
+        bot.send_message(message.chat.id, "Введите сумму дохода:")
+        bot.register_next_step_handler(message, process_income_amount)
+
+    elif message.text == "Редактировать":
+        edit_income(message)
+    elif message.text == "Удалить":
+        delete_income(message)
+        #bot.send_message(message.chat.id, "Введите индекс дохода для удаления:")
+        #bot.register_next_step_handler(message, delete_income)
+    elif message.text == "Назад":
+        start(message)
+
+
+def add_user(telegram_id):
+    cur.execute("SELECT * FROM clients WHERE telegram_id = ?",
+                (str(telegram_id),))
+    user = cur.fetchone()
+
+    if user is None:
+        cur.execute("INSERT INTO clients (telegram_id) VALUES (?)",
+                    (str(telegram_id),))
+        con.commit()
+
+
+def add_category(name):
+    cur.execute("INSERT INTO categories (name) VALUES (?)",
+                name)
+    con.commit()
+
+
+def add_income(amount, description, client_id):
+    cur.execute('''INSERT INTO incomes 
+                (amount, description, client_id) VALUES (?, ?, ?)''',
+                (amount, description, client_id))
+    con.commit()
+
+
+def add_income_handler(message):
+    bot.send_message(message.chat.id, "Введите сумму дохода:")
+    bot.register_next_step_handler(message, process_income_amount)
+
+
+def process_income_amount(message):
+    amount = float(message.text)
+    bot.send_message(message.chat.id, "Введите описание дохода:")
+    bot.register_next_step_handler(message, process_income_description, amount)
+
+
+def process_income_description(message, amount):
+    description = message.text
+    add_income(amount, description, message.chat.id)
+    bot.send_message(message.chat.id, "Доход успешно добавлен!")
+    start(message)
+
+
+def edit_income(message):
+    cur.execute(
+        '''
+        SELECT id, amount, description, date_added FROM incomes 
+        ORDER BY id DESC 
+        LIMIT 10
+        '''
+    )
+    list_income = cur.fetchall()
+    if not list_income:
+        bot.send_message(message.chat.id, "Нет записей для редактирования.")
+        return
+    response = "Последние 10 записей:\n"
+    for record in list_income:
+        response += f"ID: {record[0]}, " \
+                    f"Сумма: {record[1]}, " \
+                    f"Описание: {record[2]}, " \
+                    f"Дата: {record[3]}\n"
+    bot.send_message(message.chat.id, response)
+    bot.send_message(message.chat.id,
+                     "Введите ID записи, которую хотите редактировать:")
+    bot.register_next_step_handler(message, process_edit_id)
+
+
+def process_edit_id(message):
+    income_id = int(message.text)
+    cur.execute('SELECT * FROM incomes WHERE id = ?', (income_id,))
+    choice_income = cur.fetchone()
+    if not choice_income:
+        bot.send_message(message.chat.id, "Запись с таким ID не найдена.")
+        return
+    bot.send_message(message.chat.id, "Введите новую сумму:")
+    bot.register_next_step_handler(message, process_new_amount, income_id)
+
+
+def process_new_amount(message, income_id):
+    new_amount = float(message.text)
+    bot.send_message(message.chat.id, "Введите новое описание:")
+    bot.register_next_step_handler(message, finalize_edit, income_id,
+                                   new_amount)
+
+
+def finalize_edit(message, income_id, new_amount):
+    new_description = message.text
+    cur.execute(
+        '''
+        UPDATE incomes
+        SET amount = ?, description = ?, date_added = datetime('now')
+        WHERE id = ?
+        ''',
+        (new_amount, new_description, income_id))
+    con.commit()
+    bot.send_message(message.chat.id, "Доход успешно обновлен!")
+    start(message)
+
+
+def delete_income(message):
+    cur.execute(
+        '''
+        SELECT id, amount, description, date_added FROM incomes 
+        ORDER BY id DESC 
+        LIMIT 10
+        '''
+    )
+    list_income = cur.fetchall()
+    if not list_income:
+        bot.send_message(message.chat.id, "Нет записей для удаления.")
+        return
+    response = "Последние 10 записей:\n"
+    for record in list_income:
+        response += f"ID: {record[0]}, " \
+                    f"Сумма: {record[1]}, " \
+                    f"Описание: {record[2]}, " \
+                    f"Дата: {record[3]}\n"
+    bot.send_message(message.chat.id, response)
+    bot.send_message(message.chat.id,
+                     "Введите ID записи, которую хотите удалить:")
+    bot.register_next_step_handler(message, process_delete_id)
+
+
+def process_delete_id(message):
+    income_id = int(message.text)
+    cur.execute('SELECT * FROM incomes WHERE id = ?', (income_id,))
+    choice_income = cur.fetchone()
+    if not choice_income:
+        bot.send_message(message.chat.id, "Запись с таким ID не найдена.")
+        return
+    cur.execute(
+        '''
+            DELETE FROM incomes
+            WHERE id = ?
+        ''',
+        (income_id,))
+    con.commit()
+    bot.send_message(message.chat.id, "Доход успешно удален!")
+    start(message)
 
 def main():
     bot.polling(none_stop=True)
--- database.py
+++ database.py
@@ -2,32 +2,56 @@ import sqlite3
 
 
 def db_connect():
-    return sqlite3.connect('finance_bot.db')
+    return sqlite3.connect('finance_bot.db', check_same_thread=False)
 
 
 def create_tables():
     con = db_connect()
     cur = con.cursor()
     cur.execute('''
-                CREATE TABLE IF NOT EXITS incomes(
+                CREATE TABLE IF NOT EXISTS clients(
+                    id INTEGER PRIMARY KEY,
+                    telegram_id TEXT NOT NULL UNIQUE
+                )
+    ''')
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS categories(
+                    id INTEGER PRIMARY KEY,
+                    name TEXT NOT NULL
+                )
+    ''')
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS incomes(
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    amount REAL,
+                    amount REAL NOT NULL,
                     description TEXT,
-                    category TEXT,
-                    date TEXT
+                    date_added TEXT DEFAULT (datetime('now')),
+                    client_id INTEGER,
+                    FOREIGN KEY (client_id) REFERENCES clients(id)
                 )
                 ''')
     cur.execute('''
-                CREATE TABLE IF NOT EXITS expenses(
+                CREATE TABLE IF NOT EXISTS expenses(
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    amount REAL,
+                    amount REAL NOT NULL,
                     description TEXT,
-                    category TEXT,
-                    date TEXT
+                    date_added TEXT DEFAULT (datetime('now')),
+                    category_id INTEGER,
+                    client_id INTEGER,
+                    FOREIGN KEY (category_id) REFERENCES categories(id),
+                    FOREIGN KEY (client_id) REFERENCES clients(id)
                 )
                 ''')
     con.commit()
     con.close()
 
 
-create_tables()
+def main():
+    create_tables()
+    print('Таблицы успешно созданы')
+
+
+if __name__ == '__main__':
+    main()
+
+
