f04e98e282262c8362f1887b7107fe728a6032c1
Vyacheslav <127335939+slavakyrlan@users.noreply.github.com>
2024-10-12 22:19:08 +0300

    Т9 update code

--- bot.py
+++ bot.py
@@ -83,6 +83,8 @@ def send_action_keyboard(message, action_type):
         keyboard.add("Добавить", "Редактировать", "Удалить",
                      "Добавить категорию", "Назад")
         text = "Выберите действие с расходом:"
+    else:
+        return start(message)
 
     bot.send_message(
         chat_id=message.chat.id,
@@ -190,7 +192,7 @@ def edit_income(message):
     list_income = cur.fetchall()
     if not list_income:
         bot.send_message(message.chat.id, "Нет записей для редактирования.")
-        return
+        return start(message)
     response = "Последние 10 записей:\n"
     for record in list_income:
         response += f"ID: {record[0]}, " \
@@ -210,8 +212,7 @@ def edit_income(message):
 def edit_income_by_id(message):
     """Изменение записи Дохода по id"""
     if message.text == "Вернутся в меню":
-        start(message)
-        return
+        return start(message)
     try:
         income_id = int(message.text)
     except ValueError:
@@ -264,7 +265,7 @@ def delete_income(message):
     list_income = cur.fetchall()
     if not list_income:
         bot.send_message(message.chat.id, "Нет записей для удаления.")
-        return
+        return start(message)
     response = "Последние 10 записей:\n"
     for record in list_income:
         response += f"ID: {record[0]}, " \
@@ -283,8 +284,7 @@ def delete_income(message):
 def process_delete_id(message):
     """Удаление записи Дохода"""
     if message.text == "Вернутся в меню":
-        start(message)
-        return
+        return start(message)
     try:
         income_id = int(message.text)
     except ValueError:
@@ -296,7 +296,7 @@ def process_delete_id(message):
     choice_income = cur.fetchone()
     if not choice_income:
         bot.send_message(message.chat.id, "Запись с таким ID не найдена.")
-        return
+        return start(message)
     cur.execute(
         '''
             DELETE FROM incomes
@@ -323,8 +323,8 @@ def process_expense_description(message, amount):
         bot.send_message(message.chat.id, "Выберите категорию:",
                          reply_markup=category_keyboard)
         bot.register_next_step_handler(
-            message, lambda msg: process_expense_category(
-                msg, amount, description, categories))
+            message, process_expense_category,
+            amount, description, categories)
     else:
         bot.send_message(
             message.chat.id,
@@ -382,7 +382,7 @@ def edit_expense(message):
     if not list_expense:
         bot.send_message(message.chat.id,
                          "Нет записей расходов для редактирования.")
-        return
+        return start(message)
     response = "Последние 10 записей:\n"
     for record in list_expense:
         response += f"ID: {record[0]}, " \
@@ -402,8 +402,7 @@ def edit_expense(message):
 def process_edit_id_expense(message):
     """Выбор записи расхода по id для редактирования"""
     if message.text == "Вернутся в меню":
-        start(message)
-        return
+        return start(message)
     try:
         expense_id = int(message.text)
     except ValueError:
@@ -502,7 +501,7 @@ def delete_expense(message):
     list_income = cur.fetchall()
     if not list_income:
         bot.send_message(message.chat.id, "Нет записей для удаления.")
-        return
+        return start(message)
     response = "Последние 10 записей:\n"
     for record in list_income:
         response += f"ID: {record[0]}, " \
@@ -521,8 +520,7 @@ def delete_expense(message):
 def process_delete_id_expense(message):
     """Удаление записи Расхода"""
     if message.text == "Вернутся в меню":
-        start(message)
-        return
+        return start(message)
     try:
         income_id = int(message.text)
     except ValueError:
@@ -534,7 +532,7 @@ def process_delete_id_expense(message):
     choice_income = cur.fetchone()
     if not choice_income:
         bot.send_message(message.chat.id, "Запись с таким ID не найдена.")
-        return
+        return start(message)
     cur.execute(
         '''
             DELETE FROM expenses
@@ -566,7 +564,7 @@ def handle_period_selection(message):
         resize_keyboard=True,
         one_time_keyboard=True)
 
-    markup.add("Таблица", "Диаграмма", "График")
+    markup.add("Таблица", "Диаграмма", "График", "Меню")
     bot.send_message(message.chat.id,
                      "Выберите формат отображения:",
                      reply_markup=markup)
@@ -584,6 +582,8 @@ def handle_format_selection(message, user_periods):
         show_chart_statistics(message, user_periods)
     elif selected_format == "График":
         show_graph_statistics(message, user_periods)
+    elif message.text == "Меню":
+        start(message)
     else:
         bot.send_message(message.chat.id, "Неверный выбор формата. Пожалуйста, выберите снова.")
         handle_period_selection(message, user_periods)
@@ -602,6 +602,12 @@ def show_table_statistics(message, user_periods):
         ORDER BY e.amount DESC
         LIMIT 5
         """, con)
+
+    total_expenses = top_expenses['amount'].sum()
+    if not total_expenses:
+        bot.send_message(message.chat.id,
+                         f"Записей расходов нет")
+        return start(message)
     file_name = 'top_expenses.xlsx'
     top_expenses.to_excel(file_name, index=True)
 
@@ -627,9 +633,13 @@ def show_graph_statistics(message, user_periods):
     # преобразование столбца date_added и amount к нужному формату
     choice_expenses['date_added'] = pd.to_datetime(
         choice_expenses['date_added'])
+    total_expenses = choice_expenses['amount'].sum()
+    if not total_expenses:
+        bot.send_message(message.chat.id,
+                         f"Записей расходов нет")
+        return start(message)
     choice_expenses['amount'] = pd.to_numeric(choice_expenses['amount'],
                                               errors='coerce')
-
     choice_expenses = choice_expenses.sort_values(by='date_added')
     file_name = 'expenses.xlsx'
     choice_expenses.to_excel(file_name, index=True)
@@ -652,7 +662,6 @@ def show_graph_statistics(message, user_periods):
     with open(graph_file_name, 'rb') as graph_file:
         bot.send_photo(message.chat.id, graph_file)
 
-    total_expenses = choice_expenses['amount'].sum()
     bot.send_message(message.chat.id,
                      f"Всего расходов за выбранный период: {total_expenses}")
 
@@ -675,6 +684,12 @@ def show_chart_statistics(message, user_periods):
     # Получаем категории по id
     category_expenses['category'] = category_expenses['category_id'].apply(
         get_category_name)
+
+    total_expenses = category_expenses['total_amount'].sum()
+    if not total_expenses:
+        bot.send_message(message.chat.id,
+                         f"Записей расходов нет")
+        return start(message)
     plt.figure(figsize=(10, 6))
     plt.pie(
         category_expenses['total_amount'],
